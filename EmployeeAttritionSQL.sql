957b9fa447eb5657acbb17e136bb400a13bb1466
shailysahay <shailysahay.13@gmail.com>
2023-02-03 13:06:35 -0500

    Update EmployeeAttritionSQL.sql

--- EmployeeAttritionSQL.sql
+++ EmployeeAttritionSQL.sql
@@ -1,47 +1,33 @@
 /*
-EMPLOYEEID, SATISFACTION_LEVEL, LAST_EVALUATION, NUMBER_OF_PROJECTS
-AVG_MONTHLY_HOURS
-TIME_SPEND_COMPANY
-WORK_ACCIDENT
-TURNOVER
-PROMOTION_5YEARS
-DEPARTMENT
-SALARY_RANGE
+
+SQL Queries used for Tableau
+
 */
 
-SELECT * FROM EmployeeAttrition
-/
-
-SELECT salary_range,turnover, count(*) 
-FROM EmployeeAttrition
-GROUP BY salary_range, turnover
-ORDER BY turnover, salary_range
-/
 
 
--- Employee Attrition Percentage
+-- 1)
+-- ########### Employee Attrition Percentage ############
 
 SELECT 
-    ROUND((MAX((SELECT COUNT(*)
-    FROM EmployeeAttrition e1
-    WHERE turnover = 1))/ COUNT(*)) * 100,2) AS AttritionPercentage
+    COUNT(*) AS Total_Employees, 
+        MAX((SELECT COUNT(*) FROM EmployeeAttrition e1
+        WHERE turnover = 1)) as Turnover_Employees,
+        ROUND((MAX((SELECT COUNT(*)
+        FROM EmployeeAttrition e1
+        WHERE turnover = 1))/ COUNT(*)) * 100,2) AS Attrition_Percentage
 FROM EmployeeAttrition e2
---GROUP BY 1
-
--- NOTE: Why we added Group By 2
--- Because subqueries are not treated as "constant"s by the compiler when it looks at aggregation queries (COUNT). 
--- So, it's like an expression which is not in the group by clause. SO it throws error
--- 2 ways to resolve:
---  1. Add GROUP BY the subquery column 'GROUP BY 1'
---  2. Add MAX on the subquery column
 /
 
 
--- Breakdown of 'employee turnover count' by Salary Range
 
--- Using JOIN and Subquery
+-- 2)
+-- ########### Attrition Rate of employees based on Salary range and turnover ###########
 
-    SELECT TotalEmp.SALARY_RANGE,  ROUND((TurnoverEmpCount/TotalEmpCount)* 100,2) AS TurnoverPercentage
+
+-- Using JOIN and Subquery --
+
+    SELECT TotalEmp.SALARY_RANGE,  ROUND((TurnoverEmpCount/TotalEmpCount)* 100,2) AS Attrition_Rate
     FROM
         (SELECT SALARY_RANGE, count(turnover) as TotalEmpCount
         FROM EmployeeAttrition
@@ -55,39 +41,38 @@ FROM EmployeeAttrition e2
     
 /
 
--- Using Partition By
-SELECT  DISTINCT SALARY_RANGE, turnover, AttritionPercentage
+-- Using Partition By --
+
+SELECT  DISTINCT SALARY_RANGE, turnover, Attrition_Rate
 FROM
     (SELECT SALARY_RANGE, turnover,
         ROUND((Count(*) OVER (PARTITION BY SALARY_RANGE, turnover)/ 
         (Count(*) OVER (PARTITION BY SALARY_RANGE)))* 100,2) 
-        as AttritionPercentage
+        as Attrition_Rate
     FROM EmployeeAttrition)
 WHERE turnover = 1
 
 /
 
 
-
--- Using CTE and Partition By
+-- Using CTE and Partition By --
 
 WITH t1 AS 
- (SELECT SALARY_RANGE, turnover, Count(*) AS TurnoverEmpCount 
+ (SELECT turnover, SALARY_RANGE, Count(*) AS Turnover_Count 
   FROM EmployeeAttrition
   GROUP BY SALARY_RANGE, turnover)
-SELECT SALARY_RANGE, turnover, TurnoverEmpCount, 
-       ROUND(((TurnoverEmpCount)/(SUM(TurnoverEmpCount) OVER (PARTITION BY SALARY_RANGE)))* 100,2) as AttritionPercentage -- no integer divide!
-FROM t1;
-
+SELECT turnover,SALARY_RANGE, Turnover_Count, 
+       ROUND(((Turnover_Count)/(SUM(Turnover_Count) OVER (PARTITION BY SALARY_RANGE)))* 100,2) as PercentEmployees -- no integer divide!
+FROM t1
+ORDER BY turnover, SALARY_RANGE;
 /
 
 
-SELECT SALARY_RANGE, Count(*) OVER (SALARY_RANGE) AS n 
-        --(0.0+Count(*) OVER w_user_rating)/(Count(*) OVER (PARTITION BY User)) AS pct
-FROM EmployeeAttrition
-/
+-- 3)
+-- ########### Attrition rate of employees based on Satisfaction range and turnover ###########
 
--- Breakdown of 'employee turnover count' by Satisfaction Range
+
+-- Creating 'Satisfaction Range' groups - 'high', 'medium', 'low'
 
 WITH SatisfactionRangeTable as
 (   
@@ -101,19 +86,47 @@ WITH SatisfactionRangeTable as
     FROM EmployeeAttrition
 )
 
-SELECT DISTINCT SatisfactionRange, turnover,
+
+-- Calculating attrition rate for each group
+
+SELECT DISTINCT turnover, SatisfactionRange, 
     ROUND((Count(*) OVER (PARTITION BY SatisfactionRange, turnover)/ 
     (Count(*) OVER (PARTITION BY SatisfactionRange)))* 100,2) 
-    AS SatisfactionAttritionPct
+    AS PercentEmployees
 FROM SatisfactionRangeTable
 ORDER BY turnover, SatisfactionRange
 
 /
-        
--- Count of employees based on Satisfaction range and turnover
+
+
+
+
+-- 4)      
+-- ########### Attrition Rate of employees based on Time spent in company and turnover ###########
+
+SELECT TotalEmp.TIME_SPEND_COMPANY,  ROUND((TurnoverEmpCount/TotalEmpCount)* 100,2) AS Attrition_Rate
+FROM
+    (SELECT TIME_SPEND_COMPANY, count(turnover) as TotalEmpCount
+    FROM EmployeeAttrition
+    GROUP BY TIME_SPEND_COMPANY) TotalEmp
+JOIN
+    (SELECT TIME_SPEND_COMPANY, count(turnover) as TurnoverEmpCount
+    FROM EmployeeAttrition
+    WHERE Turnover = 1
+    GROUP BY TIME_SPEND_COMPANY) TurnoverEmp
+ON  TotalEmp.TIME_SPEND_COMPANY = TurnoverEmp.TIME_SPEND_COMPANY
+/    
+
+
+
+
+-- 5)
+-- ########### Add 'Satisfaction range' column to the original table ###########
+
 WITH SatisfactionRangeTable as
 (   
-    SELECT EmployeeID,turnover,
+    SELECT EMPLOYEEID, LAST_EVALUATION, NUMBER_OF_PROJECTS, AVG_MONTHLY_HOURS, 
+    TIME_SPEND_COMPANY, WORK_ACCIDENT, TURNOVER, PROMOTION_5YEARS, DEPARTMENT, SALARY_RANGE,
         CASE 
             WHEN SATISFACTION_LEVEL >= 0.0 AND SATISFACTION_LEVEL <= 0.3 THEN 'low'
             WHEN SATISFACTION_LEVEL > 0.3 AND SATISFACTION_LEVEL <= 0.6 THEN 'medium'
@@ -123,8 +136,6 @@ WITH SatisfactionRangeTable as
     FROM EmployeeAttrition
 )
 
-SELECT COUNT(*), SatisfactionRange, turnover
-FROM SatisfactionRangeTable
-GROUP BY SatisfactionRange, turnover
-ORDER BY SatisfactionRange, turnover
+SELECT * FROM SatisfactionRangeTable
 /
+
